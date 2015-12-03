f0875839ca7cc3d1d3f90bd6acee095627665e4b
taweewong <it58070045@office365.it.kmitl.ac.th>
2015-12-03 18:56:34 +0700

    Gen_line
    now can call BTS20xx.txt file and create graph from BTS20xx.txt

--- Gen_Line.py
+++ Gen_Line.py
@@ -1,20 +1,23 @@
+"""BTS line graph."""
 import pygal
 def bar_graph():
     """Return bar graph as svg file."""
-    lst = ['mor chit', 11, 12, 13, 14, 20, 10, 5, 6, 11, 15, 19, 14]
-    chart = pygal.Line()
-    chart.title = (lst[0])
-    chart.add('Jan', lst[1])
-    chart.add('Feb', lst[2])
-    chart.add('Mar', lst[3])
-    chart.add('Apr', lst[4])
-    chart.add('May', lst[5])
-    chart.add('Jun', lst[6])
-    chart.add('Jul', lst[7])
-    chart.add('Aug', lst[8])
-    chart.add('Sep', lst[9])
-    chart.add('Oct', lst[10])
-    chart.add('Nov', lst[11])
-    chart.add('Dec', lst[12])
-    chart.render_to_file("BarBTS.svg")
+    with open('BTS.txt') as bts:
+        for data in csv.reader(bts):
+            lst = data[0].split()
+            chart = pygal.Line()
+            chart.title = lst[0]
+            chart.add('Jan', int(lst[1]))
+            chart.add('Feb', int(lst[2]))
+            chart.add('Mar', int(lst[3]))
+            chart.add('Apr', int(lst[4]))
+            chart.add('May', int(lst[5]))
+            chart.add('Jun', int(lst[6]))
+            chart.add('Jul', int(lst[7]))
+            chart.add('Aug', int(lst[8]))
+            chart.add('Sep', int(lst[9]))
+            chart.add('Oct', int(lst[10]))
+            chart.add('Nov', int(lst[11]))
+            chart.add('Dec', int(lst[12]))
+            chart.render_to_file("BarBTS.svg")
 bar_graph()
