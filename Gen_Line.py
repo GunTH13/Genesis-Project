86b5e5e6f5ed2fd80546d9bb11d344758dc038ab
taweewong <it58070045@office365.it.kmitl.ac.th>
2015-12-03 19:09:19 +0700

    Gen_line
    fix graph

--- Gen_Line.py
+++ Gen_Line.py
@@ -1,23 +1,17 @@
 """BTS line graph."""
 import pygal
+import csv
 def bar_graph():
     """Return bar graph as svg file."""
-    with open('BTS.txt') as bts:
+    with open('BTS20xx.txt') as bts:
         for data in csv.reader(bts):
             lst = data[0].split()
             chart = pygal.Line()
             chart.title = lst[0]
-            chart.add('Jan', int(lst[1]))
-            chart.add('Feb', int(lst[2]))
-            chart.add('Mar', int(lst[3]))
-            chart.add('Apr', int(lst[4]))
-            chart.add('May', int(lst[5]))
-            chart.add('Jun', int(lst[6]))
-            chart.add('Jul', int(lst[7]))
-            chart.add('Aug', int(lst[8]))
-            chart.add('Sep', int(lst[9]))
-            chart.add('Oct', int(lst[10]))
-            chart.add('Nov', int(lst[11]))
-            chart.add('Dec', int(lst[12]))
+            chart.x_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',\
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            chart.add(lst[0], [int(lst[1]), int(lst[2]), int(lst[3]),
+            int(lst[4]), int(lst[5]), int(lst[6]), int(lst[7]), int(lst[8]),\
+            int(lst[9]), int(lst[10]), int(lst[11]), int(lst[12])])
             chart.render_to_file("BarBTS.svg")
 bar_graph()
