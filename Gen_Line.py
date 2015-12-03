35ed8663422bed2fdcc6cafef1ac290215dd9714
GunTH13 <it58070031@office365.it.kmitl.ac.th>
2015-12-03 19:17:46 +0700

    Name lst[0]
    fix output name

--- Gen_Line.py
+++ Gen_Line.py
@@ -13,5 +13,5 @@ def bar_graph():
             chart.add(lst[0], [int(lst[1]), int(lst[2]), int(lst[3]),
             int(lst[4]), int(lst[5]), int(lst[6]), int(lst[7]), int(lst[8]),\
             int(lst[9]), int(lst[10]), int(lst[11]), int(lst[12])])
-            chart.render_to_file("BarBTS.svg")
+            chart.render_to_file(lst[0]+"BarBTS.svg")
 bar_graph()
