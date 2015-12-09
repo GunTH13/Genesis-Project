f56746df102d261870aa9c36a59c77006309be67
taweewong <it58070045@office365.it.kmitl.ac.th>
2015-12-09 20:48:29 +0700

    Add Line Sum
    Add Gen_Line_Sum for graph sum in2 550-2558

--- Gen_Line.py
+++ Gen_Line.py
@@ -4,16 +4,16 @@ import csv
 
 def bar_graph():
     """Return bar graph as svg file."""
-    year = int(input())
-    with open(str(year)+'.txt') as bts:
+    year = input()
+    with open(year+'.txt') as bts:
         for data in csv.reader(bts):
             lst = data[0].split()
             chart = pygal.Line()
             chart.title = lst[0]
             chart.x_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',\
             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            chart.add(str('25')+str(year), [int(lst[1]), int(lst[2]), int(lst[3]),
+            chart.add(str('25')+year, [int(lst[1]), int(lst[2]), int(lst[3]),
             int(lst[4]), int(lst[5]), int(lst[6]), int(lst[7]), int(lst[8]),\
             int(lst[9]), int(lst[10]), int(lst[11]), int(lst[12])])
-            chart.render_to_file(lst[0]+str(year)+".svg")
+            chart.render_to_file(lst[0]+year+".svg")
 bar_graph()
