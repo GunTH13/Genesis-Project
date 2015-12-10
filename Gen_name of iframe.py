1873d75bc29b93f46634a8baf46428aacc396b0e
GunTH13 <it58070031@office365.it.kmitl.ac.th>
2015-12-10 21:44:32 +0700

    New Gen_iframe

--- Gen_name of iframe.py
+++ Gen_name of iframe.py
@@ -1,11 +1,10 @@
 """Gen HTML for iframe"""
 def html_iframe(year, name, num):
+    print("---------------------------------")
     for i in range(1, num+1):
         keep = "%02i"%i
-        print("---------------------------------")
         print('<div align="center">')
         print('<iframe class="centre" src="http://gun.infiteam.in.th/Genesis/New%20Doc/'+year+'\/'+name+'/'+str(keep)+'.svg" scrolling="no" frameborder="no" height="600" width="700" align="centre" >')
         print('</iframe></div>')
         print('<p> </p>')
-        print('---------------------------------')
 html_iframe(input(), input(), int(input()))
